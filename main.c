497e993fc68fe36e1cdc5d31a97a056b02ac6ddd
Ernest Blake <59033052+Striker172@users.noreply.github.com>
2026-01-26 13:30:05 -0600

    Starter Files
    as

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: travi
+ *
+ * Created on January 26, 2026, 1:26 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=JuniorDesign.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/JuniorDesign.X.production.hex
